edea0038c4f8afa025ff62cb8efbc69254052bac
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-31 16:41:10 +0000

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/letdummer/scripts.git
+
+mv header_make_script/header_make.sh $HOME
+rm -rf header_make_script
+
+RC_FILE="$HOME/.zshrc"
+
+if [ "$(uname)" != "Darwin" ]; then
+	RC_FILE="$HOME/.bashrc"
+	if [[ -f "$HOME/.zshrc" ]]; then
+		RC_FILE="$HOME/.zshrc"
+	fi
+fi
+
+if [ $(cat $RC_FILE | grep 'header_make.sh' | wc -l) -eq 0 ] 
+then
+	echo -e "\nalias ham=\"bash $HOME/header_make.sh\"" >> "$RC_FILE"
+fi
+
+exec "$SHELL"
